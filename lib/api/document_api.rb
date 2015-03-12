0bbb7069e720f7541ec6ea01429c361f0e60bdd2
kadotami <mondamin.kqfb@gmail.com>
2015-03-13 01:03:41 +0900

    add post,put,delete method

--- lib/api/document_api.rb
+++ lib/api/document_api.rb
@@ -1,7 +1,11 @@
 class Document_API < Grape::API
   resource "documents" do
-
-    # ex) http://localhost:3000/api/v1/books
+    helpers do
+      def document_params
+        ActionController::Parameters.new(params).permit(:user_id, :content, :title)
+      end
+    end
+    # ex) http://localhost:3000/api/v1/document
     desc "returns all documents"
     get do
       Document.all
@@ -10,11 +14,45 @@ class Document_API < Grape::API
     desc "return a document"
     params do
       requires :id, type: Integer
-      optional :title, type: String
     end
-    # http://localhost:3000/api/v1/books/{:id}
+    # http://localhost:3000/api/document/{:id}
     get ':id' do
       Document.find(params[:id])
+    end
+
+    desc "create a document"
+    params do
+      requires :content, type: String
+      requires :title, type: String
+      requires :user_id, type: Integer
+    end
+    # http://localhost:3000/api/v1/document
+    post do
+      document = Document.new(document_params)
+      document.save
+    end
+
+    desc "edit a document"
+    params do
+      optional :content, type: String
+      optional :title, type: String
+    end
+    # http://localhost:3000/api/v1/document
+    put ':id' do
+      document = Document.find(params[:id])
+      document.content = params[:content] if params[:content].present?
+      document.title = params[:title] if params[:title].present?
+      document.save
+    end
+
+    desc "edit a document"
+    params do
+      requires :id, type: Integer
+    end
+    # http://localhost:3000/api/v1/document
+    delete ':id' do
+      document = Document.find(params[:id])
+      document.destroy
     end
   end
 end
