0a4128792040e5687130f3f27262ba20a6b5137a
kadotami <mondamin.kqfb@gmail.com>
2015-04-05 02:21:05 +0900

    edit func in user_api

--- lib/api/user_api.rb
+++ lib/api/user_api.rb
@@ -2,7 +2,7 @@ class User_API < Grape::API
   resource "users" do |variable|
     helpers do
       def document_params
-        ActionController::Parameters.new(params).permit(:login_id, :name, :password, :password_confirmation, :card_id)
+        ActionController::Parameters.new(params).permit(:login_id, :name, :password, :password_confirmation, :admin_flag, :card_id)
       end
     end
 
@@ -26,26 +26,37 @@ class User_API < Grape::API
       requires :name, type: String
       requires :password, type: String
       requires :password_confirmation, type: String
-      optional :auth_flag, type: Boolean, default: false
+      optional :admin_flag, type: Boolean, default: false
       optional :card_id, type: Integer
     end
     post do
       user = User.new(document_params)
-      user.save
+      unless user.save
+        error!("error",400)
+      end
     end
 
     desc "edit user"
     params do
-      requires :login_id, type: String
-      requires :name, type: String
-      requires :password, type: String
-      requires :password_confirmation, type: String
-      optional :auth_flag, type: Boolean, default: false
+      optional :name, type: String
+      optional :password, type: String
+      optional :password_confirmation, type: String
+      optional :admin_flag, type: Boolean, default: false
       optional :card_id, type: Integer
     end
-    patch do
-      user = User.new(document_params)
-      user.save
+    patch ':id', requirements: { id: /[0-9]*/ } do
+      user = User.find(params[:id])
+      error!("you can not edit this user", 403) unless user.id == current_user.id
+      user.name = params[:name] if params[:name].present?
+      user.password = params[:password] if params[:password].present?
+      user.password_confirmation = params[:password_confirmation] if params[:password_confirmation].present?
+      user.admin_flag = params[:admin_flag] if params[:admin_flag].present?
+      user.card_id = params[:card_id] if params[:card_id].present?
+      if user.save
+        present user, Entities::User
+      else
+        error!("error",400)
+      end
     end
   end
 end
